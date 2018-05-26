71532e04d71102077dce916f8b2b2efdc7d7d68a
Vidhur Voora <lab.nirvana@gmail.com>
2018-05-26 13:42:49 -0700

    Robot Assets + Weapon Manager

---
+++ Assets/_Project/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants  {
+
+    //Gun
+    public const string Pistol = "Pistol";
+    public const string AssaultRifle = "AssaultRifle";
+    public const string Shotgun = "Shotgun";
+}
---
+++ Assets/_Project/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour {
+    private OVRInput.Controller thisController;
+    public bool leftHand = false;
+    private bool menuIsSwipable;
+    private float menuStickX;
+    public ObjectMenuManager menuManager;
+
+    // Use this for initialization
+    void Start() {
+        if (leftHand)
+        {
+            thisController = OVRInput.Controller.LTouch;
+        }
+        else
+        {
+            thisController = OVRInput.Controller.RTouch;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!leftHand)
+        {
+            menuStickX = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, thisController).x;
+            if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick, thisController))
+            {
+                if (menuStickX < 0.45f && menuStickX > -0.45f)
+                {
+                    menuIsSwipable = true;
+                }
+                if (menuIsSwipable)
+                {
+                    if (menuStickX >= 0.45f)
+                    {
+                        swipeRight();
+                        menuIsSwipable = false;
+                    }
+                    else if (menuStickX <= -0.45f)
+                    {
+                        swipeLeft();
+                        menuIsSwipable = false;
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void swipeLeft()
+    {
+        menuManager.MenuLeft();
+    }
+
+    private void swipeRight()
+    {
+        menuManager.MenuRight();
+    }
+}
