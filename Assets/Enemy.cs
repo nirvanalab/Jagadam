d1456623f6414e6ecdcd89b2351a3480294bc37f
Vidhur Voora <lab.nirvana@gmail.com>
2018-05-29 21:29:43 -0700

    Added Enemies with basic nav mesh agent

---
+++ Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField]
+    public string enemyType;
+
+    public int health;
+    public int range;
+    public float fireRate = 2;
+
+    public Transform fireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private bool isDead;
+    public GameObject player;
+    private float lastFiredTime;
+
+	// Use this for initialization
+	void Start () {
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //player = GameObject.FindGameObjectWithTag("Payer").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isDead)
+        {
+            return;
+        }
+
+        transform.LookAt(player.transform);
+        agent.SetDestination(player.transform.position);
+
+        float distanceBetweenEnemyAndPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if(distanceBetweenEnemyAndPlayer < range && ((Time.time - lastFiredTime)>fireRate))
+        {
+            lastFiredTime = Time.time;
+            Fire();
+        }
+	}
+
+    private void Fire()
+    {
+        Debug.Log("Fire");
+    }
+}
