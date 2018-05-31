a70eaceeef20cd843ab54984f62272311b271192
Vidhur Voora <lab.nirvana@gmail.com>
2018-05-30 21:46:27 -0700

    Added enemy weapons and corresponding logic

--- Assets/_Project/Scripts/Enemy.cs
+++ Assets/_Project/Scripts/Enemy.cs
@@ -7,12 +7,16 @@ public class Enemy : MonoBehaviour {
     [SerializeField]
     public string enemyType;
 
+    [SerializeField]
+    public GameObject enemyWeapon;
+
     public int health;
     public int range;
     public float fireRate = 2;
 
     public Transform fireSpot;
     UnityEngine.AI.NavMeshAgent agent;
+    public Animator robot;
 
     private bool isDead;
     public GameObject player;
@@ -45,6 +49,10 @@ public class Enemy : MonoBehaviour {
 
     private void Fire()
     {
+        GameObject weapon = Instantiate(enemyWeapon);
+        weapon.transform.position = fireSpot.transform.position;
+        weapon.transform.rotation = fireSpot.transform.rotation;
+       robot.Play("Fire");
         Debug.Log("Fire");
     }
 }
---
+++ Assets/_Project/Scripts/Missile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+    public float speed = 30f;
+    public int damage = 10;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("destroyMissile");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+
+    IEnumerator destroyMissile()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
+    }
+}
