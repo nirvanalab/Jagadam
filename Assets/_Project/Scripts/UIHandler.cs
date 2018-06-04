2e1554583f866cf70e41599a7af89086af1049f4
Vidhur Voora <lab.nirvana@gmail.com>
2018-06-03 22:04:26 -0700

    Added Main Menu UI

---
+++ Assets/_Project/Scripts/UIHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class UIHandler : MonoBehaviour {
+
+    public void ButtonBegin()
+    {
+        Debug.Log("Begin Button Clicked");
+        SceneManager.LoadScene("Battle");
+    }
+
+    public void ButtonExit()
+    {
+        Debug.Log("Exit Button Clicked");
+        Application.Quit();
+    }
+}
