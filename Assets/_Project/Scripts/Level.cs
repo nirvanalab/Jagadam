fb59780950b30e527806a46969cc59973e1382d8
Vidhur Voora <lab.nirvana@gmail.com>
2018-06-04 22:55:45 -0700

    Added Zombie asset

--- Assets/_Project/Scripts/Level.cs
+++ Assets/_Project/Scripts/Level.cs
@@ -76,7 +76,7 @@ public class Level : MonoBehaviour {
     {
         while (!isLevelOver)
         {
-            yield return new WaitForSeconds(10.0f);
+            yield return new WaitForSeconds(12.0f);
             SpawnEnemies();
         }
     }
